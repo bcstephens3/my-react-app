d18508bce14b45aa443a04b6cf25cb7f80a6251e
Brandon Stephens <bstephens22@eil-server.cba.ua.edu>
2023-02-01 14:42:29 -0600

    lol

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+export default function Main()
+{
+    return(
+        <div>
+            <h1>this is the main</h1>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar()
+{
+    return(
+        <nav>
+            <img src="./logo192.png"></img>
+            <h1>ReactFacts</h1>
+            <h2>Proj 1</h2>
+        </nav>
+    )
+}
