fa63de384fe22b01a8d6d921a65044f09f47806c
Brandon Stephens <bstephens22@eil-server.cba.ua.edu>
2023-02-01 16:35:29 -0600

    added stuff

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,14 @@
 export default function Main()
 {
     return(
-        <div>
-            <h1>this is the main</h1>
-        </div>
+        <main className="main-background">
+            <h1 className="main-txt">Fun Facts about react</h1>
+            <ul className="main-list">
+                <li>Build</li>
+                <li>Fun</li>
+                <li>Bright</li>
+                <li>Funny fun fun</li>
+            </ul>
+        </main>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ export default function Navbar()
 {
     return(
         <nav>
-            <img src="./logo192.png"></img>
-            <h1>ReactFacts</h1>
-            <h2>Proj 1</h2>
+            <img src="./logo192.png" className="nav-img"></img>
+            <h1 className="nav-txt1">ReactFacts</h1>
+            <h2 className="nav-txt2">Proj 1</h2>
         </nav>
     )
 }
